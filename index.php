82f2af4a5151bf14d1b31588f1ba61496a4a6f19
David Petringa <david@ajaxtransport.com>
2017-09-11 20:36:59 -0400

    page layout, first draft.

--- index.php
+++ index.php
@@ -1,1 +1,60 @@
-<h1>Project 1</h1>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>Home - <?php echo TITLE; ?></title>
+
+		<meta charset="UTF-8">
+		<meta name="description" content=" add description here">
+		<meta name="keywords" content="addd keywords here">
+		<meta name="author" content="David Petringa">
+
+    <link rel="shortcut icon" href="./images/favicon.ico">
+
+    <link rel = "stylesheet" href = "http://www.dukesnuz.com/css_libs/dukes_normalize.css"/>
+    <style>
+        #wrapper{
+          background-color: beige;
+          margin: auto;
+          width: 80%;
+          margin-top: 25px;
+        }
+    </style>
+    <script src="http://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+
+		</head>
+
+		<body>
+      <div id ="wrapper">
+
+        <header>
+          <h1>banner my name</h1>
+        </header>
+
+		      <section class="content">
+            <h2>h2</h2>
+            <div class="picture">
+              <!--
+                <img src="http://www.dukesnuz.com/images/harvard_id_picture_5.jpg" alt="David" width="150" height="150">
+              -->
+          </div>
+
+            <div class="about">
+                <p>about me</p>
+            </div>
+
+		        </section>
+
+		<section class="quotes">
+      <header>
+        <h2>Quotes</h2>
+      </header>
+   </section>
+
+  <footer>
+    afooter
+  </footer>
+
+  <div><!--end wrapper div-->
+		</body>
+</html>
