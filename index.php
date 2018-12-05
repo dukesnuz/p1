a8be0b2fa0bdceb5bbbc2ae63aa04851da668854
David Petringa <david@ajaxtransport.com>
2018-12-05 11:55:22 -0500

    added stat counter

--- index.php
+++ index.php
@@ -12,78 +12,94 @@ $title = 'Project 1 | David Petringa';
 <html lang="en">
 <head>
 
-    <title>
-        <?php
-        if (isset($title)) {
-            echo $title;
-        } else {
-            echo 'Project 1';
-        }
-        ?>
-    </title>
+  <title>
+    <?php
+    if (isset($title)) {
+      echo $title;
+    } else {
+      echo 'Project 1';
+    }
+    ?>
+  </title>
 
-    <meta charset="UTF-8">
-    <meta name="description" content="HES - Dynamic Web Applications - Project One">
-    <meta name="keywords" content="html, css, php">
-    <meta name="author" content="David Petringa">
+  <meta charset="UTF-8">
+  <meta name="description" content="HES - Dynamic Web Applications - Project One">
+  <meta name="keywords" content="html, css, php">
+  <meta name="author" content="David Petringa">
 
-    <link rel="shortcut icon" href="http://www.dukesnuz.com/images/favicon.ico">
-    <link rel = "stylesheet" href = "http://www.dukesnuz.com/css_libs/dukes_normalize.css"/>
-    <link rel = "stylesheet" href = "./css/p1.css"/>
+  <link rel="shortcut icon" href="http://www.dukesnuz.com/images/favicon.ico">
+  <link rel = "stylesheet" href = "http://www.dukesnuz.com/css_libs/dukes_normalize.css"/>
+  <link rel = "stylesheet" href = "./css/p1.css"/>
 
 </head>
 <body>
 
-    <header class="banner">
-        <h1>David Petringa</h1>
-        <h2>Dynamic Web Applications - Project One</h2>
-    </header>
+  <header class="banner">
+    <h1>David Petringa</h1>
+    <h2>Dynamic Web Applications - Project One</h2>
+  </header>
 
-    <div id ="wrapper">
-        <section class="content">
+  <div id ="wrapper">
+    <section class="content">
 
-            <header>
-                <h2>About Me</h2>
-            </header>
+      <header>
+        <h2>About Me</h2>
+      </header>
 
-            <div class ="picture">
-                <img src="http://www.dukesnuz.com/images/harvard_id_picture_5.jpg" alt="David" width="150" height="150">
-            </div>
+      <div class ="picture">
+        <img src="http://www.dukesnuz.com/images/harvard_id_picture_5.jpg" alt="David" width="150" height="150">
+      </div>
 
-            <div class="about">
-                <!--Print the value of the $about variable-->
-                <p><?php echo $about; ?></p>
-            </div>
+      <div class="about">
+        <!--Print the value of the $about variable-->
+        <p><?php echo $about; ?></p>
+      </div>
 
-        </section>
+    </section>
 
-        <section class="quotes">
+    <section class="quotes">
 
-            <header>
-                <h2>Quotes</h2>
-            </header>
+      <header>
+        <h2>Quotes</h2>
+      </header>
 
-            <!--call the functon to print the quotes-->
-            <?php echo getQuotes($quotes); ?>
+      <!--call the functon to print the quotes-->
+      <?php echo getQuotes($quotes); ?>
 
-        </section>
-    </div><!--END wrapper div-->
+    </section>
+  </div><!--END wrapper div-->
 
-    <footer>
+  <footer>
 
-        <div class = "linkedinButton">
-            <a href="https://www.linkedin.com/in/davidpetringa/" target="blank">Let's be friends on Linkedin</a>
-        </div>
+    <div class = "linkedinButton">
+      <a href="https://www.linkedin.com/in/davidpetringa/" target="blank">Let's be friends on Linkedin</a>
+    </div>
 
-        <ul>
-            <li>School: Harvard Extension</li>
-            <li>Class: Dynamic Web Applications</li>
-            <li>Assignment: Project one</li>
-            <li>Student: David Petringa</li>
-            <li>Coded: September 2017</li>
-        </ul>
+    <ul>
+      <li>School: Harvard Extension</li>
+      <li>Class: Dynamic Web Applications</li>
+      <li>Assignment: Project one</li>
+      <li>Student: David Petringa</li>
+      <li>Coded: September 2017</li>
+    </ul>
 
-    </footer>
+  </footer>
 
-</body>
-</html>
+  <!-- Default Statcounter code for Harvard Extension DWA
+  http://http//www.dukesnuz.com -->
+  <script type="text/javascript">
+  var sc_project=11889370;
+  var sc_invisible=1;
+  var sc_security="6ab59e53";
+  </script>
+  <script type="text/javascript"
+  src="https://www.statcounter.com/counter/counter.js"
+  async></script>
+  <noscript><div class="statcounter"><a title="Web Analytics"
+    href="http://statcounter.com/" target="_blank"><img
+    class="statcounter"
+    src="//c.statcounter.com/11889370/0/6ab59e53/1/" alt="Web
+    Analytics"></a></div></noscript>
+    <!-- End of Statcounter Code -->
+  </body>
+  </html>
